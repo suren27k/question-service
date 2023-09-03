15268aafd4f6b45a62539bea35b8c8fcb74a243e
suren27k <surendar_new@hotmail.com>
2023-09-04 00:08:51 +0530

    Load Balancing Check

--- src/main/java/com/suren/questionservice/controller/QuestionController.java
+++ src/main/java/com/suren/questionservice/controller/QuestionController.java
@@ -6,9 +6,11 @@ import com.suren.questionservice.entity.QuestionWrapper;
 import com.suren.questionservice.entity.QuizResponse;
 import com.suren.questionservice.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,6 +19,10 @@ public class QuestionController
 {
 	@Autowired
 	QuestionService questionService;
+
+	@Autowired
+	Environment environment;
+
 	@GetMapping("allQuestions")
 	public ResponseEntity<List<Question>> getAllQuestions()
 	{
@@ -44,6 +50,9 @@ public class QuestionController
 	@PostMapping("getQuestions")
 	public ResponseEntity<List<QuestionWrapper>> getQuestionsForQuiz(@RequestBody List<Integer> questionIds)
 	{
+		//Load balancing check
+		System.out.println(environment.getProperty("local.server.port") + "--> "+ new Date().toString());
+
 		return questionService.getQuestionsForQuiz(questionIds);
 	}
 
